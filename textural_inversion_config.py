e95615aabb2361d48a079f342eb21993b56c85e8
Yusuke Urakami <yu2126@columbia.edu>
2023-06-26 18:15:31 +0900

    changed name

--- textural_inversion_config.py
+++ textural_inversion_config.py
@@ -1,8 +1,8 @@
 
 textural_inversion_file_dict = \
     {
-            "/home/urakamiy/repos/diffusers/examples/textual_inversion/textual_inversion_riverplate-v1.4": "learned_embeds-steps-15000.bin",
-            "/home/urakamiy/repos/diffusers/examples/textual_inversion/textual_inversion_petite-oseille-v1.4": "learned_embeds-steps-15000.bin",
+            "/home/urakamiy/repos/diffusers/examples/textual_inversion/textual_inversion_plate-v1.4": "learned_embeds-steps-15000.bin",
+            "/home/urakamiy/repos/diffusers/examples/textual_inversion/textual_inversion_leaf-v1.4": "learned_embeds-steps-15000.bin",
             }
 
 
